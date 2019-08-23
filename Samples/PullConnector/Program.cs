80245fe35055a16cd1c04a9b511347ebb2c8e5e4
Einar Ingebrigtsen <einar@dolittle.com>
2019-08-23 15:58:56 +0200

    Up and running conceptually

---
+++ Samples/PullConnector/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Dolittle.TimeSeries.Runtime.Connectors.Grpc;
+using Grpc.Core;
+using Microsoft.Extensions.Hosting;
+
+namespace PullConnector
+{
+    class Program
+    {
+        static void HandleStreams(AsyncDuplexStreamingCall<PullConnectorGetData, PullConnectorData> stream)
+        {
+            Task.Run(async() =>
+            {
+                Console.WriteLine("Wait for messages");
+                while (await stream.ResponseStream.MoveNext(CancellationToken.None))
+                {
+                    Console.WriteLine("Message received on client");
+                    
+                }
+            });
+
+            Task.Run(async() =>
+            {
+                for( ;; )
+                {
+                    Console.WriteLine("Write message to server");
+                    await stream.RequestStream.WriteAsync(new Dolittle.TimeSeries.Runtime.Connectors.Grpc.PullConnectorGetData());
+                    //await stream.RequestStream.CompleteAsync();
+                    await Task.Delay(1000);
+                }
+
+            });
+
+
+        }
+
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Connect");
+            var channel = new Channel("0.0.0.0", 50053, ChannelCredentials.Insecure);
+            var client = new Dolittle.TimeSeries.Runtime.Connectors.Grpc.PullConnectors.PullConnectorsClient(channel);
+            var metadata = new Metadata
+            { { "clientId", Guid.NewGuid().ToString() }
+            };
+
+            Console.WriteLine("Register");
+            var stream = client.Register(metadata);
+            HandleStreams(stream);
+
+            var hostBuilder = new HostBuilder();
+            var host = hostBuilder.Build();
+
+            await host.RunAsync();
+
+        }
+    }
+}
