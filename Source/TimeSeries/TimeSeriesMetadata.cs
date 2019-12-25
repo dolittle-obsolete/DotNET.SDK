cdb1fa4b1518510ce9bd3eafe737acfdfebe14f3
Einar Ingebrigtsen <einar@dolittle.com>
2019-12-25 13:40:58 +0100

    Static code analysis fixes

--- Source/TimeSeries/TimeSeriesMetadata.cs
+++ Source/TimeSeries/TimeSeriesMetadata.cs
@@ -1,26 +1,24 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+// Copyright (c) Dolittle. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace Dolittle.TimeSeries
 {
     /// <summary>
-    /// Represents the metadata associated with a <see cref="TimeSeries"/>
+    /// Represents the metadata associated with a <see cref="TimeSeries"/>.
     /// </summary>
     public class TimeSeriesMetadata
     {
         /// <summary>
-        /// Initializes a new instance of <see cref="TimeSeriesMetadata"/>
+        /// Initializes a new instance of the <see cref="TimeSeriesMetadata"/> class.
         /// </summary>
-        /// <param name="timeSeries">The <see cref="TimeSeries"/> the metadata is for</param>
+        /// <param name="timeSeries">The <see cref="TimeSeries"/> the metadata is for.</param>
         public TimeSeriesMetadata(TimeSeriesId timeSeries)
         {
             TimeSeries = timeSeries;
         }
 
         /// <summary>
-        /// Gets the <see cref="TimeSeries"/> the metadata is for
+        /// Gets the <see cref="TimeSeries"/> the metadata is for.
         /// </summary>
         public TimeSeriesId TimeSeries { get; }
     }
